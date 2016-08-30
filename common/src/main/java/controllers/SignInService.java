55a80f40a24deb34bf28488c52bd58667ac56117
Dzmitry Ihnatsyeu <dzmitry.ihnatsyeu@pegasus.ae>
2016-08-30 17:31:00 +0300

    modified Sign In service

--- common/src/main/java/controllers/SignInService.java
+++ common/src/main/java/controllers/SignInService.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import app_context.properties.G4Properties;
 import http.requests.SignInRequest;
 import json.RsClient;
 import org.apache.log4j.Logger;
@@ -10,11 +11,12 @@ class SignInService {
 
     private RsClient rsClient = new RsClient();
     private Logger log = Logger.getLogger(SignInService.class);
+    private final String host = G4Properties.getRunProperties().getApplicationURL();
 
     Response signIn(String name, String password){
         SignInRequest signInRequest = new SignInRequest();
         signInRequest.setName(name);
         signInRequest.setPassword(password);
-        return rsClient.post(context.environment().getSigintHost() + signInRequest.getURI(), signInRequest);
+        return rsClient.post(host + signInRequest.getURI(), signInRequest);
     }
 }
