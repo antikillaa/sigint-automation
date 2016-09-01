949958788d17a6ed6cb386d367df21450a3e1f47
admin <tower83@gmail.com>
2016-09-01 19:06:51 +0300

    update source with clear name

--- api_tests/src/main/java/steps/APISourceSteps.java
+++ api_tests/src/main/java/steps/APISourceSteps.java
@@ -14,6 +14,7 @@ import services.SourceService;
 import utils.RandomGenerator;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static conditions.Conditions.equals;
@@ -93,9 +94,10 @@ public class APISourceSteps extends APISteps {
         Source source = Entities.getSources().getLatest();
 
         // update all fields
-        SourceType type = source.getType();
-        source = source.generate();
-        source.setType(type);
+        SourceType sourceType = source.getType();
+        source = source.generate()
+                .setType(sourceType)
+                .setName(sourceType.toLetterCode() + source.getRecordType() + new Date().getTime());
 
         int responseCode = service.update(source);
 
