ee01063ba4c0f8656a6409d3966e0af39b3c892e
Dzmitry Ihnatsyeu <dzmitry.ihnatsyeu@pegasus.ae>
2016-09-29 18:56:52 +0300

    missed files

---
+++ common/src/main/java/jenkins/JenkinsJobsNames.java
@@ -0,0 +1,25 @@
+package jenkins;
+
+import java.util.HashMap;
+
+public class JenkinsJobsNames {
+    
+    private static HashMap<String, JenkinsJob> jobHashMap = new HashMap<>();
+    
+    static  {
+        jobHashMap.put("develop", JenkinsJob.DEVELOP);
+        jobHashMap.put("master", JenkinsJob.MASTER);
+        
+    }
+    
+    public static String getJobName(String standBranch) {
+        JenkinsJob jenkinsJob = jobHashMap.get(standBranch);
+        if (jenkinsJob == null) {
+            return null;
+        } else {
+            return jenkinsJob.getJobName();
+        }
+        
+    }
+    
+}
