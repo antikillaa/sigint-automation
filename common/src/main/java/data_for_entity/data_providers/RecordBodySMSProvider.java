868ba5207d935bdf64a115d095efe9b398b2fe50
admin <tower83@gmail.com>
2016-10-14 17:22:34 +0300

    fix text field on SMS

--- common/src/main/java/data_for_entity/data_providers/RecordBodySMSProvider.java
+++ common/src/main/java/data_for_entity/data_providers/RecordBodySMSProvider.java
@@ -4,15 +4,14 @@ import model.RecordType;
 import org.apache.commons.lang.RandomStringUtils;
 
 public class RecordBodySMSProvider extends DependencyDataProvider {
-    
+
     @Override
     public Object generate(int length) {
         DependencyData dependencyData = getDependencyData();
-        String type = (String)dependencyData.getData("type");
-    
-        if (type.equals(RecordType.SMS)) {
+        String type = (String) dependencyData.getData("type");
+
+        if (type.equals(RecordType.SMS.name())) {
             return RandomStringUtils.randomAlphabetic(30);
         } else return null;
-        
     }
 }
