106115799ee190248c738035cc5c27e1fbf7986e
Dzmitry Ihnatsyeu <dzmitry.ihnatsyeu@pegasus.ae>
2016-10-18 16:15:05 +0300

    add missing files

---
+++ common/src/main/java/data_for_entity/DataTypeManager.java
@@ -0,0 +1,38 @@
+package data_for_entity;
+
+import data_for_entity.data_providers.*;
+import data_for_entity.data_types.FieldDataType;
+
+import java.util.HashMap;
+
+public class DataTypeManager {
+    
+    static EntityDataProvider getDataTypeProvider(FieldDataType fieldDataType) {
+        return DataTypesProviders.providerHashMap.get(fieldDataType);
+        
+    }
+    
+    public static void putToProviders(FieldDataType dataType, EntityDataProvider provider) {
+        DataTypesProviders.providerHashMap.put(dataType, provider);
+    }
+    
+    
+    private static class DataTypesProviders {
+        
+        private static HashMap<FieldDataType, EntityDataProvider> providerHashMap = new HashMap<>();
+        
+        static {
+            providerHashMap.put(FieldDataType.DIGIT, new IntData());
+            providerHashMap.put(FieldDataType.ALPHANUMERIC, new AlphaNumeric());
+            providerHashMap.put(FieldDataType.BOOLEAN, new BooleanData());
+            providerHashMap.put(FieldDataType.COLLECTION, null);
+            providerHashMap.put(FieldDataType.DATE, new DateData());
+            providerHashMap.put(FieldDataType.NUMERIC, new Numeric());
+            providerHashMap.put(FieldDataType.STRING, new StringData());
+            providerHashMap.put(FieldDataType.UNKNOWN, null);
+            providerHashMap.put(FieldDataType.LONG, new LongData());
+        }
+        
+    }
+    
+}
