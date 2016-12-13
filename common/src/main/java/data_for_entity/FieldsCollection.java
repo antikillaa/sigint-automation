f9a8e65fdeae137a7d0d545502afaf9422a6f038
admin <tower83@gmail.com>
2016-12-13 18:37:15 +0300

    add javadoc

--- common/src/main/java/data_for_entity/FieldsCollection.java
+++ common/src/main/java/data_for_entity/FieldsCollection.java
@@ -13,20 +13,21 @@ import java.util.stream.Collectors;
  * to get filtered data
  */
 final class FieldsCollection {
-    
-    
+
     private List<ObjectField> fields;
-    
+
     /**
      * Gets all declared fields
+     *
      * @return list of {@link ObjectField} within this type.
      */
     List<ObjectField> getFields() {
         return Collections.unmodifiableList(fields);
     }
-    
+
     /**
      * Set param object Type
+     *
      * @param objectType class of object for inspection
      */
     void collectFieldsByType(Class<?> objectType) {
@@ -34,6 +35,14 @@ final class FieldsCollection {
         this.fields = fieldsToObjectFields(listFields);
     }
 
+    /**
+     * Collect all the fields up the hierarchy
+     *
+     * @param startClass      started class
+     * @param exclusiveParent The exclusiveParent class is provided to prevent the retrieval of fields from Object.
+     *                        If may be null if you DO want the Object fields.
+     * @return All the fields up the hierarchy
+     */
     private Iterable<Field> getFieldsUpTo(Class<?> startClass, Class<?> exclusiveParent) {
 
         List<Field> currentClassFields = new ArrayList<>(Arrays.asList(startClass.getDeclaredFields()));
@@ -47,53 +56,54 @@ final class FieldsCollection {
         }
         return currentClassFields;
     }
-    
-    
+
+
     /**
      * Internal method to transfer list of {@link Field} objects to list
      * of {@link ObjectField} objects.
+     *
      * @param fields list of {@link Field} objects.
      * @return list of {@link ObjectField} objects.
      */
     private List<ObjectField> fieldsToObjectFields(List<Field> fields) {
         return fields.stream().map(ObjectField::new).collect(Collectors.toList());
     }
-    
+
     /**
      * Creates a {@link FieldsFilter} instance
+     *
      * @return {@link FieldsFilter}
      */
     FieldsFilter createFieldsFilter() {
-        return  new FieldsFilter();
+        return new FieldsFilter();
     }
-    
+
     /**
      * Helps to filter list of objects by criteria.
      */
-     class FieldsFilter {
-    
+    class FieldsFilter {
+
         /**
          * Filter list of object by required attributes. Required attribute if received from
          * {@link FieldOptionsManager}
-         
+         *
          * @return filtered {@link ObjectField} list.
          */
         List<ObjectField> filterByRequired() {
             return fields.stream().filter(field -> !new FieldOptionsManager(field.getField()).shouldBeIgnored())
                     .collect(Collectors.toList());
         }
-    
+
         /**
          * Filter list of dependent fields against this field.
+         *
          * @param dependentFieldsNames Array of dependent fields' names.
          * @return list of {@link ObjectField} filtered by names provided in dependentFieldsNames
          */
         List<ObjectField> filterByDependentFields(String[] dependentFieldsNames) {
-            return  fields.stream().filter(field ->  Arrays.asList(dependentFieldsNames).contains(field.getName()))
+            return fields.stream().filter(field -> Arrays.asList(dependentFieldsNames).contains(field.getName()))
                     .collect(Collectors.toList());
-            
         }
-        
-        
+
     }
 }
