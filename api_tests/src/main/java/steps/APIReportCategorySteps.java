0ad8f1013ba3c97e48125af74d6a9abd84739d01
Anton Yakutovich <Anton.Yakutovich@pegasus.ae>
2017-02-08 13:30:40 +0300

    Move 'API.Create report category' to the appropriate story

--- api_tests/src/main/java/steps/APIReportCategorySteps.java
+++ api_tests/src/main/java/steps/APIReportCategorySteps.java
@@ -1,7 +1,9 @@
 package steps;
 
 import abs.EntityList;
+import app_context.entities.Entities;
 import http.OperationResult;
+import http.OperationsResults;
 import model.ReportCategory;
 import org.apache.log4j.Logger;
 import org.jbehave.core.annotations.Then;
@@ -12,20 +14,40 @@ import services.ReportCategoryService;
 @SuppressWarnings("unchecked")
 public class APIReportCategorySteps extends APISteps {
 
-    private ReportCategoryService service = new ReportCategoryService();
-    private static Logger LOGGER = Logger.getLogger(APIReportCategorySteps.class);
+    private static final Logger log = Logger.getLogger(APIReportCategorySteps.class);
+    private ReportCategoryService reportCategoryService = new ReportCategoryService();
 
     @When("I send get list of report categories request")
     public void getReportCategoryList() {
-        OperationResult<EntityList<ReportCategory>> result = service.list();
+        OperationResult<EntityList<ReportCategory>> result = reportCategoryService.list();
         context.put("reportCategoryEntityList", result.getResult());
     }
 
     @Then("Report categories list size more than $count")
     public void reportCategoryListShouldBeMoreThan(String count) {
-        EntityList<ReportCategory> reportCategories = context.get("reportCategoryEntityList", EntityList.class);
+        EntityList<ReportCategory> reportCategories =
+                context.get("reportCategoryEntityList", EntityList.class);
 
         Assert.assertTrue(reportCategories.getEntities().size() > 0);
     }
 
+    @When("I create new report category")
+    public void createReportCategory() {
+        ReportCategory reportCategory = objectInitializer.randomEntity(ReportCategory.class);
+
+        OperationResult<ReportCategory> operationResult = reportCategoryService.add(reportCategory);
+        OperationsResults.setResult(operationResult);
+
+        context.put("reportCategory", operationResult.getResult());
+    }
+
+    @Then("Report category is correct")
+    public void reportCategoryIsCorrect() {
+        ReportCategory savedCategory = Entities.getReportCategories().getLatest();
+        ReportCategory responcedCategory = context.get("reportCategory", ReportCategory.class);
+
+        Assert.assertEquals("hidden mismatch", savedCategory.getHidden(), responcedCategory.getHidden());
+        Assert.assertEquals("option values mismatch", savedCategory.getValues(), responcedCategory.getValues());
+        Assert.assertEquals("category name mismatch", savedCategory.getName(), responcedCategory.getName());
+    }
 }
--- api_tests/src/main/resources/stories/API_ReportCategory.story
+++ api_tests/src/main/resources/stories/API_ReportCategory.story
@@ -6,6 +6,12 @@ As a user
 I want to perform an action
 So that I can achieve a business goal
 
+Scenario: API.Create report category
+Given I sign in as admin user
+When I create new report category
+Then Request is successful
+And Report category is correct
+
 Scenario: API.Get list of report categories
 Given I sign in as admin user
 When I send get list of report categories request
