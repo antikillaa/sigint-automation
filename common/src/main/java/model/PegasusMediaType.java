c992c90978f9152738a95061dcd8c4fc4aa18214
Anton Yakutovich <Anton.Yakutovich@pegasus.ae>
2017-07-13 16:10:14 +0300

    Upload .md files with application/json media type

--- common/src/main/java/model/PegasusMediaType.java
+++ common/src/main/java/model/PegasusMediaType.java
@@ -7,6 +7,7 @@ public class PegasusMediaType {
     public static final String PEGASUS_JSON_V1 = "application/vnd.ae.pegasus.v1+json";
     public static final String TEXT_CSV = "text/csv";
     public static final MediaType TEXT_CSV_TYPE = new MediaType("text", "csv");
+    public static final MediaType APPLICATION_JSON_TYPE = new MediaType("application", "json");
     public static final String MS_EXCEL = "application/vnd.ms-excel";
     public static final MediaType MS_EXCEL_TYPE = new MediaType("application", "vnd.ms-excel");
     public static final MediaType AUDIO = new MediaType("audio", "wav");
