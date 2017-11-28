20f327b1b8e7e27b656187c6895bff0ac6fa161e
Anton Yakutovich <Anton.Yakutovich@pegasus.ae>
2017-11-28 16:58:42 +0300

    Change passwords according to CB-7275

--- common/src/main/java/model/UserPassword.java
+++ common/src/main/java/model/UserPassword.java
@@ -1,9 +1,9 @@
 package model;
 
-import static utils.StringUtils.capitalizeFirstLetter;
-
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+import static utils.StringUtils.capitalizeFirstLetter;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
@@ -12,46 +12,59 @@ public class UserPassword {
   private String name;
   private String oldPassword;
   private String newPassword;
-  private String id;
+  private String adminPassword;
 
-  public UserPassword(User user) {
-    this.name = capitalizeFirstLetter(user.getName());
-    this.oldPassword = user.getPassword();
-    this.newPassword = user.getNewPassword();
-
-    // save User's id for password update via /api/auth/users/{id} call
-    this.id = user.getId();
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
+  private UserPassword(UserPasswordBuilder builder) {
+    this.name = builder.name;
+    this.oldPassword = builder.oldPassword;
+    this.newPassword = builder.newPassword;
+    this.adminPassword = builder.adminPassword;
   }
 
   public String getName() {
     return name;
   }
 
-  public void setName(String name) {
-    this.name = name;
-  }
-
   public String getOldPassword() {
     return oldPassword;
-  }
-
-  public void setOldPassword(String oldPassword) {
-    this.oldPassword = oldPassword;
   }
 
   public String getNewPassword() {
     return newPassword;
   }
 
-  public void setNewPassword(String newPassword) {
-    this.newPassword = newPassword;
+  public String getAdminPassword() {
+    return adminPassword;
+  }
+
+  public static class UserPasswordBuilder {
+    private String name;
+    private String oldPassword;
+    private String newPassword;
+    private String adminPassword;
+
+    public UserPasswordBuilder name(String name) {
+      this.name = capitalizeFirstLetter(name);
+      return this;
+    }
+
+    public UserPasswordBuilder oldPassword(String oldPassword) {
+      this.oldPassword = oldPassword;
+      return this;
+    }
+
+    public UserPasswordBuilder newPassword(String newPassword) {
+      this.newPassword = newPassword;
+      return this;
+    }
+
+    public UserPasswordBuilder adminPassword(String adminPassword) {
+      this.adminPassword = adminPassword;
+      return this;
+    }
+
+    public UserPassword build() {
+      return new UserPassword(this);
+    }
   }
 }
