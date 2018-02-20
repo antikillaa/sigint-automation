56166e39f896cfbe2c733c2c2b5cee6165656156
Anton Yakutovich <Anton.Yakutovich@pegasus.ae>
2018-02-20 08:33:14 +0400

    Fix Geo model

--- common/src/main/java/model/Geo.java
+++ common/src/main/java/model/Geo.java
@@ -1,11 +1,26 @@
 package model;
 
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+import java.util.Date;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
 public class Geo {
 
+    private Date time;
     private Integer sequence;
     private List coordinates;
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
 
     public Integer getSequence() {
         return sequence;
