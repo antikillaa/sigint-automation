d8fd009561447f2e9afa5ae14390bffe71deb51b
stasnislav.peshkur <1342365Pass>
2018-06-20 16:05:13 +0300

    Added report model, Scenario for generate Report number Test for generate Report number

---
+++ common/src/main/java/ae/pegasus/framework/model/Event.java
@@ -0,0 +1,105 @@
+
+package ae.pegasus.framework.model;
+
+import com.fasterxml.jackson.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "id",
+        "order",
+        "type",
+        "source",
+        "body",
+        "metadata"
+})
+public class Event {
+
+    @JsonProperty("id")
+    private String id;
+    @JsonProperty("order")
+    private Integer order;
+    @JsonProperty("type")
+    private String type;
+    @JsonProperty("source")
+    private Source source;
+    @JsonProperty("body")
+    private String body;
+    @JsonProperty("metadata")
+    private Metadata metadata;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("id")
+    public String getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @JsonProperty("order")
+    public Integer getOrder() {
+        return order;
+    }
+
+    @JsonProperty("order")
+    public void setOrder(Integer order) {
+        this.order = order;
+    }
+
+    @JsonProperty("type")
+    public String getType() {
+        return type;
+    }
+
+    @JsonProperty("type")
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @JsonProperty("source")
+    public Source getSource() {
+        return source;
+    }
+
+    @JsonProperty("source")
+    public void setSource(Source source) {
+        this.source = source;
+    }
+
+    @JsonProperty("body")
+    public String getBody() {
+        return body;
+    }
+
+    @JsonProperty("body")
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @JsonProperty("metadata")
+    public Metadata getMetadata() {
+        return metadata;
+    }
+
+    @JsonProperty("metadata")
+    public void setMetadata(Metadata metadata) {
+        this.metadata = metadata;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
