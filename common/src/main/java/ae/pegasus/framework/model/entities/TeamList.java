b1931648ab3079565a94c7d0494ab34dd7f01460
Aleksandr Dubatovka <aleksandr@pegasus.ae>
2018-06-29 14:44:57 +0300

    fix get team by id

--- common/src/main/java/ae/pegasus/framework/model/entities/TeamList.java
+++ common/src/main/java/ae/pegasus/framework/model/entities/TeamList.java
@@ -4,10 +4,4 @@ import ae.pegasus.framework.model.Team;
 
 class TeamList extends EntityList<Team> {
 
-    @Override
-    public Team getEntity(String name) {
-        return entities.stream()
-                .filter(team -> team.getFullName().equals(name))
-                .findAny().orElse(null);
-    }
 }
