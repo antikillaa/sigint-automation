330e60a4df3cf37d8eaac0924db076e991b6da13
stasnislav.peshkur <1342365Pass>
2018-08-01 18:23:25 +0300

    Refactored Add report test Added asserts for created report

--- common/src/main/java/ae/pegasus/framework/model/ReportEvent.java
+++ common/src/main/java/ae/pegasus/framework/model/ReportEvent.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@org.codehaus.jackson.annotate.JsonIgnoreProperties(ignoreUnknown = true)
+
 @JsonPropertyOrder({
         "id",
         "order",
