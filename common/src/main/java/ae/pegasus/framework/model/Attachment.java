fe90de47bbc74f6ed1c666fa17a51b30c1bf70ff
stasnislav.peshkur <1342365Pass>
2018-08-28 16:50:29 +0300

    Added event attachment model

---
+++ common/src/main/java/ae/pegasus/framework/model/Attachment.java
@@ -0,0 +1,79 @@
+
+package ae.pegasus.framework.model;
+
+import com.fasterxml.jackson.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "id",
+        "name",
+        "order",
+        "extracted"
+})
+public class Attachment {
+
+    @JsonProperty("id")
+    private String id;
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("order")
+    private Integer order;
+    @JsonProperty("extracted")
+    private Boolean extracted;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("id")
+    public String getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("order")
+    public Integer getOrder() {
+        return order;
+    }
+
+    @JsonProperty("order")
+    public void setOrder(Integer order) {
+        this.order = order;
+    }
+
+    @JsonProperty("extracted")
+    public Boolean getExtracted() {
+        return extracted;
+    }
+
+    @JsonProperty("extracted")
+    public void setExtracted(Boolean extracted) {
+        this.extracted = extracted;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
